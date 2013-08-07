1768513ad7c687f1828be17fbd2744d45e244ac4
Elmar Zander <e.zander@tu-bs.de>
2013-08-07 18:04:29 +0200

    Tests for rescaling

--- src/vis/rescale.py
+++ src/vis/rescale.py
@@ -1,7 +1,5 @@
 from numpy import log
 
-from sim.physics import Units
-
 def rescale_size(d, d0, f):
     # 1 realistic, 0 all same
     d0 = float(d0)
@@ -12,21 +10,18 @@ def rescale_size_factor(d, d0, f):
 
 
 
-def rescale_orbit(r, f):
-    # 1 realistic, 0 linearly spaced
-    r = r / Units.AU
-    if r<=0.4:
-        return r * Units.AU
-    n = log((r-0.4)/0.3)/log(2.0)
-    return (1/(3.0*f)-f/30.0)*(7.0/3*f-1+(1+f)**n) * Units.AU
-
+def rescale_orbit(r, r0, sr, dr, f):
+    #a=dr/log(sr)
+    #b=r0-a*log(r0)
+    #rn = a*log(r) + b
+    if r==0:
+        return 0
+    rn = dr*log(r/r0)/log(sr) + r0
+    if r<r0:
+        rn = max(r, rn)
+    return r**(f) * rn**(1-f)
 
 def rescale_orbit_factor(r, f):
     if r == 0:
         return 1
     return rescale_orbit(r, f) / r
-
-#rp = [0.5, 0.72, 1.0, 1.52, 2.77, 5.2, 9.54]
-#print rp
-#print [rescale_orbit(r,1) for r in rp]
-#rint [rescale_orbit(r,0.0001) for r in rp]
--- src/vis/test_rescale.py
+++ src/vis/test_rescale.py
@@ -8,7 +8,7 @@ Created on Wed Aug  7 11:30:00 2013
 from numpy import array
 from numpy.testing import assert_approx_equal, assert_almost_equal
 
-from vis.rescale import rescale_size
+from vis.rescale import rescale_size, rescale_orbit
 
 def test_rescale_size():
     assert_almost_equal( rescale_size(2.0, 3.0, 1.0), 2)
@@ -27,9 +27,16 @@ def test_rescale_size_realistic():
     assert_almost_equal(dp1, dp)
     assert_almost_equal(dp0, array(len(dp) * [1e5]))
     
+def test_rescale_orbit():
+    assert_almost_equal( rescale_orbit(2.0, 3.0, 1.5, 1.0, 1.0), 2)
+    
+    assert_almost_equal( rescale_orbit(2.0, 3.0, 1.5, 1.0, 0.0), 2)
+    assert_almost_equal( rescale_orbit(2.0/1.5, 3.0, 1.5, 0.4, 0.0), 2.2)
+    assert_almost_equal( rescale_orbit(2.0/1.5, 3.0, 1.5, 0.4, 1.0), 2.0/1.5)
+    
+    assert_almost_equal( rescale_size(2.0, 3.0, 0.0), 3)
 
-    
+def test_rescale_orbit_realistic():
+    rp = array([0.39, 0.72, 1.0, 1.52, 2.77, 5.2, 9.54, 19.2, 30.06])
+    assert_almost_equal(array([rescale_orbit(r, 10.0, 1.6, 1, 1.0) for r in rp]), rp)
 
-    
-    
-    
